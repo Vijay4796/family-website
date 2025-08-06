b858a865d3c6d88740662b76398cbedca81b9797
Vijay Malviya <100106839+Vijay4796@users.noreply.github.com>
2025-08-06 22:32:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,8 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(MONGO_URI);
+
 .then(() => {
   console.log('✅ Connected to MongoDB');
 
